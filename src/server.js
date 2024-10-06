6e1c2fdee2e6ef20666cfd7f2e9d26d1dd33b99b
Aidan Kamp <ackamp2@gmail.com>
2024-10-06 17:48:40 -0400

    all bugs fixed

--- src/server.js
+++ src/server.js
@@ -5,68 +5,66 @@ const jsonHandler = require('./jsonResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-// const urlStruct = {
-//   '/': htmlHandler.getIndex,
-//   '/style.css': htmlHandler.getCSS,
-//   notFound: jsonHandler.notFound,
-// };
+
 
 const parseBody = (request, response, handler) => {
-  const body = [];
+    const body = [];
 
-  request.on('error', (err) => {
-    console.dir(err);
-    response.statusCode = 400;
-    response.end();
-  });
+    request.on('error', (err) => {
+        console.dir(err);
+        response.statusCode = 400;
+        response.end();
+    });
 
-  request.on('data', (chunk) => {
-    body.push(chunk);
-  });
+    request.on('data', (chunk) => {
+        body.push(chunk);
+    });
 
-  request.on('end', () => {
-    const bodyString = Buffer.concat(body).toString();
-    request.body = query.parse(bodyString);
+    request.on('end', () => {
+        const bodyString = Buffer.concat(body).toString();
+        request.body = query.parse(bodyString);
 
-    // call handler function
-    handler(request, response);
-  });
+        // call handler function
+        handler(request, response);
+    });
 };
 
 // handle POST requests
 const handlePost = (request, response, parsedUrl) => {
-  // addUser
-  if (parsedUrl.pathname === '/addUser') {
-    parseBody(request, response, jsonHandler.addUser);
-  }
+    // addUser
+    if (parsedUrl.pathname === '/addUser') {
+        parseBody(request, response, jsonHandler.addUser);
+    }
 };
 
 // handle GET requests
 const handleGet = (request, response, parsedUrl) => {
-  // route to correct method based on url
-  if (parsedUrl.pathname === '/style.css') {
-    htmlHandler.getCSS(request, response);
-  } else if (parsedUrl.pathname === '/getUsers') {
-    jsonHandler.getUsers(request, response);
-  } else {
-    htmlHandler.getIndex(request, response);
-  }
+    // route to correct method based on url
+    if (parsedUrl.pathname === '/style.css') {
+        htmlHandler.getCSS(request, response);
+    } else if (parsedUrl.pathname === '/getUsers') {
+        jsonHandler.getUsers(request, response);
+    } else if (parsedUrl.pathname === '/notReal') {
+        jsonHandler.notFound(request, response);
+    } else {
+        htmlHandler.getIndex(request, response);
+    }
 };
 
 // function to handle requests
 const onRequest = (request, response) => {
-  const protocol = request.connection.encrypted ? 'https' : 'http';
-  const parsedUrl = new URL(request.url, `${protocol}://${request.headers.host}`);
+    const protocol = request.connection.encrypted ? 'https' : 'http';
+    const parsedUrl = new URL(request.url, `${protocol}://${request.headers.host}`);
 
-  if (request.method === 'POST') {
-    handlePost(request, response, parsedUrl);
-  } else {
-    handleGet(request, response, parsedUrl);
-  }
-  // return not found if neither worked
-  //return jsonHandler.notFound(request, response);
+    if (request.method === 'POST') {
+        handlePost(request, response, parsedUrl);
+    } else {
+        handleGet(request, response, parsedUrl);
+    }
+    // return not found if neither worked
+    //return jsonHandler.notFound(request, response);
 };
 
 http.createServer(onRequest).listen(port, () => {
-  console.log(`Listening on 127.0.0.1: ${port}`);
+    console.log(`Listening on 127.0.0.1: ${port}`);
 });
